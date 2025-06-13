a26aa48595c06554242d273e7ea0ce2d51418421
FriendlyGuardian <tobin.rumsey@gmail.com>
2025-06-12 18:48:16 -0600

    created the basic framework for basics.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basics.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp(name="basics", group="Linear OpMode")
+@Disabled
+public class basics extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+
+    }
+}
