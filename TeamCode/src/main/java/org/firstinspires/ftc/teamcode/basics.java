168d48e55e12b0c2bb1ed48c6e27f30f9d8c197b
FriendlyGuardian <tobin.rumsey@gmail.com>
2025-06-13 10:53:58 -0600

    it is now a tank drive!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basics.java
@@ -4,17 +4,35 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 
 @TeleOp(name="basics", group="Linear OpMode")
-@Disabled
 public class basics extends LinearOpMode {
-
+private DcMotor fl = null;
+private DcMotor bl = null;
+private DcMotor fr = null;
+private DcMotor br = null;
+private double leftJoyStickY;
+    private double rightJoyStickY;
     @Override
     public void runOpMode() {
-
-
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        bl = hardwareMap.get(DcMotor.class, "bl");
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        br = hardwareMap.get(DcMotor.class, "br");
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
+        waitForStart();
+        while(opModeIsActive()){
+            fl.setPower(gamepad1.left_stick_y);
+            bl.setPower(gamepad1.left_stick_y);
+            fr.setPower(gamepad1.right_stick_y);
+            br.setPower(gamepad1.right_stick_y);
+            telemetry.addData("your code", gamepad1.left_stick_y);
+            telemetry.update();
+        }
     }
 }
